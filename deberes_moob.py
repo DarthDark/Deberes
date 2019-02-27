94bd35663f7ff4d96c7c22321e16424c28c7a7b0
DarthDark <darthdark@debian>
2019-02-27 17:11:50 +0100

    comentario

---
+++ deberes_moob.py
@@ -0,0 +1,22 @@
+
+estado = input("Introduce estado de ánimo: ")
+
+mood = "happy"
+
+if estado == "happy":
+    print("It is great to see you happy!")
+
+elif estado == "nervioso":
+    print("Take a deep breath 3 times.")
+
+elif estado == "triste":
+    print("A llorar.")
+
+elif estado == "emocionado":
+    print("lagrimilla...")
+
+elif estado == "relajado":
+    print("Viva la vida.")
+
+else:
+    print("I don't recognize this mood")
